b835d63aeb874bd1223f9c7b7008a0a64ef429e1
Botayna-azmy <bottaynaazmy@gmail.com>
2024-05-10 12:10:00 +0100

    Implemented functionality using the useEffect hook in my TP, ensuring efficient state management and component lifecycle handling. Ready for review!

--- src/Hooks/Effect/Effect.jsx
+++ src/Hooks/Effect/Effect.jsx
@@ -1,18 +1,41 @@
 import { useEffect, useState } from "react"
 
 export default function Effect(){
-    const [counter,setCounter] = useState(0)
+    const [users,setUsers] = useState([])
+    const [inputValues, setInputValues] = useState('')
+    const [filteredUsers, setFilteredUsers] = useState([])
+     useEffect(()=>{
+            fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res =>res.json())
+            .then(data => setUsers(data))   
+           return () => {
+            return false
+           }
+          },[])
+          
+          useEffect(()=>{
+            if(users.length > 0){
+                console.log(users)
+            }
+          },[users])
+          useEffect (()=>{
+            setFilteredUsers(users)
+          },[users])
 
-    useEffect(()=>{
-        console.log("mount")
-      },[])
-    const update = () => {
-       return setCounter(prev=> prev +1)
-    }
+          useEffect((e)=>{
+            const filter = users.filter(
+            user => user.name.toLowerCase().includes(inputValues)
+                 )
+                setFilteredUsers(filter)
+          },[inputValues])
+
   return (
     <div>
-       <h1>il y a {counter}</h1> 
-        <button onClick={update}>Click</button>
+        <input type="text" id="" onInput={e=>setInputValues(e.target.value)} />
+       {filteredUsers.map (
+        user => <h2 key={user.id}>{user.name}</h2>
+       )}
+       
     </div>
   )
 }
